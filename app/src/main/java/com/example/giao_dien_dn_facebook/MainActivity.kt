4fbfab246b6842dc657c8af751cbeea04e6022c2
PhanVanHieu-Ptit <n19dccn056@student.ptithcm.edu.vn>
2021-08-20 22:03:30 +0700

    giaodien

---
+++ app/src/main/java/com/example/giao_dien_dn_facebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.giao_dien_dn_facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
